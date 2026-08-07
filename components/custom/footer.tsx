a2cb6dcd6577ea226e2f97083e23e183fb3b2f09
it254a-ux <it.254a@gmail.com>
2026-08-07 13:56:39 +0300

    Update footer text size to 10px

--- components/custom/footer.tsx
+++ components/custom/footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <footer className="w-full py-0.5 text-center">
-      <p className="text-xs leading-tight tracking-wide text-muted-foreground">
+      <p className="text-[10px] leading-tight tracking-wide text-muted-foreground">
         Powered by <span className="font-semibold text-foreground">Deriv</span>
       </p>
     </footer>
