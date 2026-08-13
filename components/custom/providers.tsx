9cacea5e0a2410c49c16887d43f191c4d12f6482
it254a-ux <it.254a@gmail.com>
2026-08-13 09:18:26 +0300

    Add ThemeBridge component to Providers

--- components/custom/providers.tsx
+++ components/custom/providers.tsx
@@ -1,10 +1,10 @@
 'use client';
-
 import { ThemeProvider } from 'next-themes';
-
+import { ThemeBridge } from './theme-bridge';
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="light" disableTransitionOnChange>
+      <ThemeBridge />
       {children}
     </ThemeProvider>
   );
